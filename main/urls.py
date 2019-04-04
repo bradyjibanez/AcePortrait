1b3f5e617a503cf762af90b6eb2a8e756d1a9104
SPP User <sppuser@spp.com>
2019-04-03 22:17:48 -0400

    Added Liking System

--- main/urls.py
+++ main/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
 	# Hunter Views
 	path('h/home', views.hunter_home, name='hunter_home'),
 	path('h/prospect/<int:prospect>', views.hunter_view, name='hunter_view'),
+	path('h/prospect/<int:prospect>/like', views.hunter_like, name='hunter_like'),
+	path('h/prospect/<int:prospect>/dislike', views.hunter_dislike, name='hunter_dislike'),
 	path('h/prospect/<int:prospect>/message', views.hunter_message, name='hunter_message'),
 	# Prospect views
 	path('p/home', views.prospect_home, name='prospect_home'),
